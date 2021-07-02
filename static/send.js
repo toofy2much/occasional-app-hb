7bb773dcad74959d1bf7aee71c58ba1298021268
Tuesday Kay <tuesday.kay@gmail.com>
2021-07-02 00:32:25 -0400

    Bootstrap styling/assign class

--- static/send.js
+++ static/send.js
@@ -6,104 +6,34 @@ $("#send").on("click",()=> {
     });
 }) 
 
-// $(".flashes").on("submit",()=> {
+$.backstretch("/img/wallpaper2.png");
 
-// })
+
 setTimeout(function() {
     $('.flashes').slideUp(300);
 }, 6000); // <-- time in milliseconds
 
 
-
-
 $('#email').click(function () {
-
-
   $(this).removeAttr('placeholder');
-
-    // if($(this).val() == ''){
-    //     $(this).attr('placeholder' , "My PlaceHolder");
-    // }
-
 });
 
 $('#phone').click(function () {
-
-
   $(this).removeAttr('placeholder');
-
-    // if($(this).val() == ''){
-    //     $(this).attr('placeholder' , "My PlaceHolder");
-    // }
-
 });
 
-
 $('#occ_date').click(function () {
-
-
   $(this).removeAttr('placeholder');
-  
-      // if($(this).val() == ''){
-      //     $(this).attr('placeholder' , "My PlaceHolder");
-      // }
-  
-  });
+});
   
 
 $('#send_dt').click(function () {
-
-
-    $(this).removeAttr('placeholder');
+  $(this).removeAttr('placeholder');
+});
   
-      // if($(this).val() == ''){
-      //     $(this).attr('placeholder' , "My PlaceHolder");
-      // }
-  
-  });
-  
-// $('.label').on('hover',() => {
-//     $(this).attr('placeholder', {'visibility': 'hidden'});
-// });
-// not working hide placeholder
-
-// for each placeholder in lable class show/hide palceholder value on hover
-// $('.label').mouseenter('show',(){
-//     $(this).attr('placeholder') 
-// });
-//   $('.label').mouseout('hide',(){
-//     $(this).attr('placeholder');
-//   });
-
-// $("").on({
-//     mouseover: function() {
-//         $("#dd").stop().show();
-//     },
-//     mouseout: function() {
-//         $("#dd").stop().hide();
-//     } })
-  
-// $('#form').on('focus', function(){
-//     if(!$(this).data('placeholder')){
-//         $(this).data('placeholder', $(this).attr('placeholder'));
-//     }
-//     $(this).attr('placeholder', "");
-// }).on('focusout', function(){
-//     if($(this).val()==""){
-//         $(this).attr('placeholder', $(this).data('placeholder'));
-//     }
-// });
-// $("tbody").slideToggle('slow');
 
 $('thead').click(function(){ 
-    //  $(this).toggleClass('.table'); 
   $(".tr_toggle").slideToggle('slow');
- 
 });
 
-
-// $('.table').mouseleave(function(){   
-// //   $(this).find('.header').toggleClass('expand'); 
-//   $(this).find('tbody tr').slideToggle('600');
-// });
 
